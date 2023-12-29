df4969edf3407e9b256af3d605af20750c5beb86
zhangpinge <50477615+warku123@users.noreply.github.com>
2023-12-29 19:54:12 +0800

    first commit

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <CodeExecutor/>
+  </div>
+</template>
+
+<script>
+import CodeExecutor from './components/CodeExecutor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CodeExecutor
+  }
+}
+</script>
+
+<style>
+/* 在这里添加全局或针对App.vue的样式 */
+</style>
---
+++ front/src/components/CodeExecutor.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <textarea v-model="code" placeholder="在这里输入代码..."></textarea>
+      <button @click="executeCode">执行</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        code: '' // 用于存储输入的代码
+      };
+    },
+    methods: {
+      executeCode() {
+        // 在这里添加执行代码的逻辑
+        console.log("执行的代码：", this.code);
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  textarea {
+      width: 50%; /* 占据界面的一半 */
+      height: 200px; /* 适当的高度 */
+      margin: 10px;
+  }
+  
+  button {
+      display: block;
+      margin: 10px;
+  }
+  </style>
+  
