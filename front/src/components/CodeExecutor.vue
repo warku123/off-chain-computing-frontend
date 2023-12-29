f3f7e7149a755e8b42c46532c440dab4b188be37
zhangpinge <50477615+warku123@users.noreply.github.com>
2023-12-29 20:31:39 +0800

    try fix bug

--- front/src/components/CodeExecutor.vue
+++ front/src/components/CodeExecutor.vue
@@ -10,40 +10,40 @@
       </div>
     </div>
 </template>
-
+  
 <script>
-export default {
-  data() {
-    return {
-      code: '', // 用于存储输入的代码
-      snapshots: [], // 存储快照图片的数组
-      intervalId: null // 存储 setInterval 的 ID
-    };
-  },
-  methods: {
-    executeCode() {
+  export default {
+    data() {
+      return {
+        code: '', // 用于存储输入的代码
+        snapshots: [], // 存储快照图片的数组
+        intervalId: null // 存储 setInterval 的 ID
+      };
+    },
+    methods: {
+      executeCode() {
+        if (this.intervalId !== null) {
+          // 如果已有计时器在运行，则清除它
+          clearInterval(this.intervalId);
+        }
+  
+        this.addSnapshot();
+  
+        // 每隔2秒添加一个新的快照
+        this.intervalId = setInterval(this.addSnapshot, 2000);
+      },
+      addSnapshot() {
+        // 添加新快照到数组，使用 require 来指定资源路径
+        this.snapshots.push(require('@/assets/state.png'));
+      }
+    },
+    beforeDestroy() {
+      // 组件销毁前清除计时器
       if (this.intervalId !== null) {
-        // 如果已有计时器在运行，则清除它
         clearInterval(this.intervalId);
       }
-
-      this.addSnapshot();
-
-      // 每隔2秒添加一个新的快照
-      this.intervalId = setInterval(this.addSnapshot, 2000);
-    },
-    addSnapshot() {
-      // 添加新快照到数组，使用 require 来指定资源路径
-      this.snapshots.push(require('@/assets/state.png'));
     }
-  },
-  beforeDestroy() {
-    // 组件销毁前清除计时器
-    if (this.intervalId !== null) {
-      clearInterval(this.intervalId);
-    }
-  }
-};
+  };
 </script>
   
 <style>
@@ -71,21 +71,17 @@ export default {
 
   .snapshot-container {
     display: flex;
-    flex-wrap: wrap;
+    flex-wrap: nowrap; /* 不换行，以便出现滚动条 */
+    overflow-x: auto; /* 当内容宽度超过容器宽度时显示滚动条 */
     gap: 10px;
     padding: 10px;
-    justify-content: flex-start;
-    align-items: flex-start;
     margin-top: 20px;
   }
 
-  .snapshot {
-    flex: 0 1 auto;
-  }
-
   .snapshot img {
-    width: 100px;
-    display: block;
+    width: 100px; /* 快照图片的大小 */
+    min-width: 100px; /* 确保图片不会被压缩 */
+    height: auto; /* 保持图片比例 */
   }
 </style>
   
