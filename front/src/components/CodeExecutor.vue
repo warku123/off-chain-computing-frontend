7f5e2e171e51e7a058efb853443dc4797a48c949
zhangpinge <50477615+warku123@users.noreply.github.com>
2023-12-29 20:27:25 +0800

    try flex

--- front/src/components/CodeExecutor.vue
+++ front/src/components/CodeExecutor.vue
@@ -3,32 +3,47 @@
       <h1>执行器测试</h1>
       <textarea v-model="code" placeholder="在这里输入代码..."></textarea>
       <button @click="executeCode">执行</button>
-      <div class="snapshots">
-        <img v-for="(snapshot, index) in snapshots" :key="index" :src="snapshot" alt="快照" />
+      <div class="snapshot-container">
+        <div class="snapshot" v-for="(snapshot, index) in snapshots" :key="index">
+          <img :src="snapshot" alt="快照" />
+        </div>
       </div>
     </div>
 </template>
-  
+
 <script>
-  export default {
-    data() {
-      return {
-        code: '', // 用于存储输入的代码
-        snapshots: [] // 存储快照图片的数组
-      };
-    },
-    methods: {
-      executeCode() {
-        // 添加快照
-        this.snapshots.push(require('@/assets/state.png')); // 替换为实际快照图片的路径
-  
-        // 每隔2秒添加一个新的快照
-        setInterval(() => {
-          this.snapshots.push(require('@/assets/state.png')); // 替换为实际快照图片的路径
-        }, 2000);
+export default {
+  data() {
+    return {
+      code: '', // 用于存储输入的代码
+      snapshots: [], // 存储快照图片的数组
+      intervalId: null // 存储 setInterval 的 ID
+    };
+  },
+  methods: {
+    executeCode() {
+      if (this.intervalId !== null) {
+        // 如果已有计时器在运行，则清除它
+        clearInterval(this.intervalId);
       }
+
+      this.addSnapshot();
+
+      // 每隔2秒添加一个新的快照
+      this.intervalId = setInterval(this.addSnapshot, 2000);
+    },
+    addSnapshot() {
+      // 添加新快照到数组，使用 require 来指定资源路径
+      this.snapshots.push(require('@/assets/state.png'));
     }
-  };
+  },
+  beforeDestroy() {
+    // 组件销毁前清除计时器
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+    }
+  }
+};
 </script>
   
 <style>
@@ -54,9 +69,23 @@
     height: 40px; /* 按钮高度固定 */
   }
 
-  .snapshots img {
-  width: 100px; /* 或者任何您希望的大小 */
-  margin: 10px;
+  .snapshot-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    padding: 10px;
+    justify-content: flex-start;
+    align-items: flex-start;
+    margin-top: 20px;
+  }
+
+  .snapshot {
+    flex: 0 1 auto;
+  }
+
+  .snapshot img {
+    width: 100px;
+    display: block;
   }
 </style>
   
