7658e7f81b3d198476f42c3b45877faad1a6a0e9
zhangpinge <50477615+warku123@users.noreply.github.com>
2023-12-29 19:59:50 +0800

    change the flex format

--- front/src/components/CodeExecutor.vue
+++ front/src/components/CodeExecutor.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="code-executor">
+      <h1>执行器测试</h1>
       <textarea v-model="code" placeholder="在这里输入代码..."></textarea>
       <button @click="executeCode">执行</button>
     </div>
@@ -22,15 +23,26 @@
   </script>
   
   <style>
+  .code-executor {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .code-executor > textarea,
+  .code-executor > button {
+    width: 80%; /* 输入框和按钮宽度相同，占据父元素宽度的 80% */
+    max-width: 500px; /* 设置一个最大宽度 */
+    margin: 10px 0;
+  }
+  
   textarea {
-      width: 50%; /* 占据界面的一半 */
-      height: 200px; /* 适当的高度 */
-      margin: 10px;
+    height: 20vh; /* 输入框高度根据视口高度变化 */
+    min-height: 100px; /* 设置最小高度 */
   }
   
   button {
-      display: block;
-      margin: 10px;
+    height: 40px; /* 按钮高度固定 */
   }
   </style>
   
